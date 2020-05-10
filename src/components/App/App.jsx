af02f7006d40f3e0b677d7548ae5823e8f147a41
Will G Piro <willgpiro@gmail.com>
2020-05-10 16:16:20 -0700

    updated test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import Resty2 from '../RESTy2/RESTy2.jsx';
 export default function App() {
   return (
     <RestyProvider>
-      <Resty2/>
+      <Resty2 />
     </RestyProvider>
   );
 }
--- src/components/RESTy2/RESTy2.jsx
+++ src/components/RESTy2/RESTy2.jsx
@@ -3,13 +3,15 @@ import Form from '../Form/Form.jsx';
 import HistoryList from '../History/HistoryList.jsx';
 import Response from '../Response/Response.jsx';
 
-const Resty2 = () => (
-  <>
-    <HistoryList />
-    <Form/>
-    <Response/>
-  </>
-);
+const Resty2 = () => {
+  return (
+    <>
+      <HistoryList />
+      <Form/>
+      <Response/>
+    </>
+  );
+};
 
 export default Resty2;
 
