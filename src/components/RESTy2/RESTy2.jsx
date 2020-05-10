3235156cd767f07e24aed119e995499a1660d47b
Will G Piro <willgpiro@gmail.com>
2020-05-10 13:39:06 -0700

    makeFetch service

--- src/components/RESTy2/RESTy2.jsx
+++ src/components/RESTy2/RESTy2.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from '../Form/Form.jsx';
 import HistoryList from '../History/HistoryList.jsx';
+import Response from '../Response/Response.jsx';
 
 const Resty2 = () => (
   <>
