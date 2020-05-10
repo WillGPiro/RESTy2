9c88f0e062f4adffa68aa4ce5f43a3449a0b0a8a
Will G Piro <willgpiro@gmail.com>
2020-05-10 13:28:09 -0700

    add Resty Provider start History Item

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import Form from '../Form/Form.jsx';
+import { RestyProvider } from '../../hooks/RestyProvider.jsx';
+import Resty2 from '../RESTy2/RESTy2.jsx';
 
 export default function App() {
-  return <Form />;
+  return (
+    <RestyProvider>
+      <Resty2/>
+    </RestyProvider>
+  );
 }
   
---
+++ src/components/RESTy2/RESTy2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../Form/Form.jsx';
+
+const Resty2 = () => {
+
+  return (
+    <>
+      <Form/>
+    </>
+  );
+};
+
+export default Resty2;
+
