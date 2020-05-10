a6f0757f149db2adfebb613e28d65d28b9985bc7
Will G Piro <willgpiro@gmail.com>
2020-05-10 12:35:16 -0700

    fixed onSubmit to onChange

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,24 +5,40 @@ import PropTypes from 'prop-types';
 const Form = ({ url, method, body, onSubmit, onChange }) => (
   <section>
     <form className="Form" onSubmit={onSubmit} >
-      <input type='text' name='url' value={url} onChange={onChange}/>
+      <input type='text' name='url' value={url} 
+        onChange={onChange}/>
 
-      <input type='radio' name='method' value='GET' checked={method === 'GET'} onSubmit={onSubmit}/>
+      <input type='radio' name='method' value='GET' 
+        checked={method === 'GET'} 
+        onChange={onChange}/>
       <label>GET:</label>
 
-      <input type='radio' name='method' value='POST' checked={method === 'POST'} onSubmit={onSubmit}/>
+      <input type='radio' name='method' value='POST' 
+        checked={method === 'POST'} 
+        onChange={onChange}/>
       <label>POST:</label>
 
-      <input type='radio' name='method' value='PUT' checked={method === 'PUT'} onSubmit={onSubmit}/>
+      <input type='radio' name='method' value='PUT' 
+        checked={method === 'PUT'} 
+        onChange={onChange}/>
       <label>PUT:</label>
 
-      <input type='radio' name='method' value='PATCH' checked={method === 'PATCH'} onSubmit={onSubmit}/>
+      <input type='radio' name='method' value='PATCH' 
+        checked={method === 'PATCH'} 
+        onChange={onChange}/>
       <label>PATCH:</label>
 
-      <input type='radio' name='method' value='DELETE' checked={method === 'DELETE'} onSubmit={onSubmit}/>
+      <input type='radio' name='method' value='DELETE' 
+        checked={method === 'DELETE'} 
+        onChange={onChange}/>
       <label>DELETE:</label>
 
       <button>GO!</button>
+      
+      <textarea name="body" value={body} 
+        onChange={onChange}>
+      </textarea>
+      
     </form>
   </section>
 );
